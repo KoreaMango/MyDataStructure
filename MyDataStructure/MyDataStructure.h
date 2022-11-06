b87e9cde8f41540829e481ab9096f2f823c64ce2
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-11-06 18:21:11 +0900

    Initial Commit

---
+++ MyDataStructure/MyDataStructure.h
@@ -0,0 +1,18 @@
+//
+//  MyDataStructure.h
+//  MyDataStructure
+//
+//  Created by 강민규 on 2022/11/06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDataStructure.
+FOUNDATION_EXPORT double MyDataStructureVersionNumber;
+
+//! Project version string for MyDataStructure.
+FOUNDATION_EXPORT const unsigned char MyDataStructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDataStructure/PublicHeader.h>
+
+
